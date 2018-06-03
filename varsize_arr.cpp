d54f570683c7ff1ee06741518179b8391ce66ad4
Lishant Sahu <lishantsahu@gmail.com>
2018-06-03 18:29:01 +0530

    variable size array using vector in c++

---
+++ varsize_arr.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> a(n);
+    
+    for(int i = 0; i < n; ++i){
+        int k;
+        cin >> k;
+        
+        a[i].resize(k);
+        for(int j = 0; j < k; ++j){
+            cin >> a[i][j];
+        }
+    }
+    
+    for(int numq = 0; numq < q; numq++){
+        int i, j;
+        cin >> i >> j;
+        cout << a[i][j] << endl;
+    }
+    return 0;
+}
